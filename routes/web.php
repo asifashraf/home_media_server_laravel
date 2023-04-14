090ff936ffdebcf08fc09fd33b89588a11cf068a
asif <asif.ashraf@tintash.com>
2023-04-14 21:50:09 +0500

    cp

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +19,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
